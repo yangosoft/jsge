2234f1341d536a8e47c433db6dac15264a08baee
DAERobotics <daerobotics@gmail.com>
2018-04-08 23:27:49 +0200

    Renamed main.html to index.html

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# JSGE 
+# JSGE
 Javascript HTML5 Game Engine
 
 [![CircleCI](https://circleci.com/gh/yangosoft/jsge.svg?style=svg)](https://circleci.com/gh/yangosoft/jsge)
 
 Very simple javascript game engine.
+
+```
+$ cd src
+$ npm i
+$ npm run server
+```  
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import 'jquery-ui/ui/widgets/selectable';
 import 'jquery-ui/ui/widgets/draggable';
 
 
-  
+
 
 import * as F from "./js/falafel/falafel";
 
@@ -27,42 +27,37 @@ module.exports = {
 
         $( "#draggable" ).draggable();
         $( "#rigthpanel" ).draggable();
-        
-        
+
+
     },
     setCanvas: function(idCanvas)
     {
         var canvas = document.getElementById(idCanvas);
-        
-        
-   
-        
-        
         var ctx = canvas.getContext('2d');
         falafel = new F.Falafel(ctx);
-        
+
         canvas.addEventListener("mousedown", (e) =>{ falafel._mouse.handleMouseDown(e) }, false);
         canvas.addEventListener("mousemove", (e) =>{ falafel._mouse.handleMouseMove(e) }, false);
         canvas.addEventListener("mouseup", (e) =>{ falafel._mouse.handleMouseUp(e) }, false);
         canvas.addEventListener("mouseout", (e) =>{ falafel._mouse.handleMouseOut(e) }, false);
-        
-        
-        
-        
+
+
+
+
         var s = new F.Scene();
         s.width = 1280;
         s.height = 720;
         s.name = "Escena";
-        
-        
+
+
         let k = new F.ImageNode();
         k.setImage("https://static.comicvine.com/uploads/original/0/40/2486696-batman_the_dark_knight_rises_the_dark_knight_rises_30411051_967_1450.jpeg");
         k.name = "img";
         let q = new F.Point(50,50);
         k.position = q;
         s.addChild(k);
-        
-        
+
+
         let i = new F.Rectangle();
         i.name = "red";
         let p = new F.Point(10,10);
@@ -72,13 +67,13 @@ module.exports = {
         i.height = 200;
         let red = new F.Color(255,0,0,1);
         i.color = red;
-        
+
         s.addChild(i);
-        
-        
-        
+
+
+
         let j = new F.Rectangle();
-        
+
         j.name = "yellow";
         p = new F.Point(20,20);
         console.log("p " +  JSON.stringify(p));
@@ -87,9 +82,9 @@ module.exports = {
         j.height = 400;
         j.color = new F.Color(255,255,0,1);
         s.addChild(j);
-        
+
         let l = new F.Rectangle();
-        
+
         l.name = "blue";
         p = new F.Point(40,40);
         console.log("p " +  JSON.stringify(p));
@@ -98,26 +93,26 @@ module.exports = {
         l.height = 40;
         l.color = new F.Color(0,255,255,1);
         i.addChild(l);
-        
-        
-        
-        
-      
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
         falafel.addChild(s);
-        
-        
+
+
         //Escena 2
-        
+
         var s2 = new F.Scene();
         s2.width = 1280;
         s2.height = 720;
         s2.name = "Escena 2";
-        
+
         k = new F.ImageNode();
         k.setImage("https://static.comicvine.com/uploads/original/0/40/2486696-batman_the_dark_knight_rises_the_dark_knight_rises_30411051_967_1450.jpeg");
         k.name = "img";
@@ -126,7 +121,7 @@ module.exports = {
         s2.addChild(k);
         falafel.addChild(s2);
         //
-        
+
         let t = new F.Label();
         p = new F.Point(300,200);
         t.position = p;
@@ -134,18 +129,16 @@ module.exports = {
         t.font = "100px Arial";
         t.fillStyle = "yellow";
         s2.addChild(t);
-        
+
         //falafel._nEditor.describeScene(falafel);
-        
-        falafel.runScene(s2._name);
-        
-        
-     
+
+        falafel.runScene(s._name);
+
+
+
         requestAnimationFrame((dt)=>{falafel.run(dt)});
-        
+
         //falafel.run();
-        
+
     }
 }
-  
-  
