746c00bd1dfb0da49bc67b1356bcaf387f6eb262
yangosoft <yangosoft@gmail.com>
2018-04-08 23:32:13 +0200

    Beautified some files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-
-
 import $ from 'jquery';
 import jquery from 'jquery';
 import 'jquery-ui';
@@ -23,122 +21,121 @@ let n = new F.NodeBase();
 
 
 module.exports = {
-    load:function() {
+  load: function() {
 
-        $( "#draggable" ).draggable();
-        $( "#rigthpanel" ).draggable();
-
-
-    },
-    setCanvas: function(idCanvas)
-    {
-        var canvas = document.getElementById(idCanvas);
-        var ctx = canvas.getContext('2d');
-        falafel = new F.Falafel(ctx);
-
-        canvas.addEventListener("mousedown", (e) =>{ falafel._mouse.handleMouseDown(e) }, false);
-        canvas.addEventListener("mousemove", (e) =>{ falafel._mouse.handleMouseMove(e) }, false);
-        canvas.addEventListener("mouseup", (e) =>{ falafel._mouse.handleMouseUp(e) }, false);
-        canvas.addEventListener("mouseout", (e) =>{ falafel._mouse.handleMouseOut(e) }, false);
+    $("#draggable").draggable();
+    $("#rigthpanel").draggable();
 
 
+  },
+  setCanvas: function(idCanvas) {
+    var canvas = document.getElementById(idCanvas);
+    var ctx = canvas.getContext('2d');
+    falafel = new F.Falafel(ctx);
 
-
-        var s = new F.Scene();
-        s.width = 1280;
-        s.height = 720;
-        s.name = "Escena";
-
-
-        let k = new F.ImageNode();
-        k.setImage("https://static.comicvine.com/uploads/original/0/40/2486696-batman_the_dark_knight_rises_the_dark_knight_rises_30411051_967_1450.jpeg");
-        k.name = "img";
-        let q = new F.Point(50,50);
-        k.position = q;
-        s.addChild(k);
-
-
-        let i = new F.Rectangle();
-        i.name = "red";
-        let p = new F.Point(10,10);
-        console.log("p " +  JSON.stringify(p));
-        i.position = p;
-        i.width = 100;
-        i.height = 200;
-        let red = new F.Color(255,0,0,1);
-        i.color = red;
-
-        s.addChild(i);
-
-
-
-        let j = new F.Rectangle();
-
-        j.name = "yellow";
-        p = new F.Point(20,20);
-        console.log("p " +  JSON.stringify(p));
-        j.position = p;
-        j.width = 200;
-        j.height = 400;
-        j.color = new F.Color(255,255,0,1);
-        s.addChild(j);
-
-        let l = new F.Rectangle();
-
-        l.name = "blue";
-        p = new F.Point(40,40);
-        console.log("p " +  JSON.stringify(p));
-        l.position = p;
-        l.width = 100;
-        l.height = 40;
-        l.color = new F.Color(0,255,255,1);
-        i.addChild(l);
+    canvas.addEventListener("mousedown", (e) => {
+      falafel._mouse.handleMouseDown(e)
+    }, false);
+    canvas.addEventListener("mousemove", (e) => {
+      falafel._mouse.handleMouseMove(e)
+    }, false);
+    canvas.addEventListener("mouseup", (e) => {
+      falafel._mouse.handleMouseUp(e)
+    }, false);
+    canvas.addEventListener("mouseout", (e) => {
+      falafel._mouse.handleMouseOut(e)
+    }, false);
 
 
 
 
+    var s = new F.Scene();
+    s.width = 1280;
+    s.height = 720;
+    s.name = "Escena";
+
+
+    let k = new F.ImageNode();
+    k.setImage("https://static.comicvine.com/uploads/original/0/40/2486696-batman_the_dark_knight_rises_the_dark_knight_rises_30411051_967_1450.jpeg");
+    k.name = "img";
+    let q = new F.Point(50, 50);
+    k.position = q;
+    s.addChild(k);
+
+
+    let i = new F.Rectangle();
+    i.name = "red";
+    let p = new F.Point(10, 10);
+    console.log("p " + JSON.stringify(p));
+    i.position = p;
+    i.width = 100;
+    i.height = 200;
+    let red = new F.Color(255, 0, 0, 1);
+    i.color = red;
+
+    s.addChild(i);
 
 
 
+    let j = new F.Rectangle();
+
+    j.name = "yellow";
+    p = new F.Point(20, 20);
+    console.log("p " + JSON.stringify(p));
+    j.position = p;
+    j.width = 200;
+    j.height = 400;
+    j.color = new F.Color(255, 255, 0, 1);
+    s.addChild(j);
+
+    let l = new F.Rectangle();
+
+    l.name = "blue";
+    p = new F.Point(40, 40);
+    console.log("p " + JSON.stringify(p));
+    l.position = p;
+    l.width = 100;
+    l.height = 40;
+    l.color = new F.Color(0, 255, 255, 1);
+    i.addChild(l);
+    falafel.addChild(s);
+
+
+    //Escena 2
+
+    var s2 = new F.Scene();
+    s2.width = 1280;
+    s2.height = 720;
+    s2.name = "Escena 2";
+
+    k = new F.ImageNode();
+    k.setImage("https://static.comicvine.com/uploads/original/0/40/2486696-batman_the_dark_knight_rises_the_dark_knight_rises_30411051_967_1450.jpeg");
+    k.name = "img";
+    q = new F.Point(50, 50);
+    k.position = q;
+    s2.addChild(k);
+    falafel.addChild(s2);
+    //
+
+    let t = new F.Label();
+    p = new F.Point(300, 200);
+    t.position = p;
+    t.text = "HELLO WORLD";
+    t.font = "100px Arial";
+    t.fillStyle = "yellow";
+    s2.addChild(t);
+
+    //falafel._nEditor.describeScene(falafel);
+
+    falafel.runScene(s._name);
 
 
 
-        falafel.addChild(s);
+    requestAnimationFrame((dt) => {
+      falafel.run(dt)
+    });
 
+    //falafel.run();
 
-        //Escena 2
-
-        var s2 = new F.Scene();
-        s2.width = 1280;
-        s2.height = 720;
-        s2.name = "Escena 2";
-
-        k = new F.ImageNode();
-        k.setImage("https://static.comicvine.com/uploads/original/0/40/2486696-batman_the_dark_knight_rises_the_dark_knight_rises_30411051_967_1450.jpeg");
-        k.name = "img";
-        q = new F.Point(50,50);
-        k.position = q;
-        s2.addChild(k);
-        falafel.addChild(s2);
-        //
-
-        let t = new F.Label();
-        p = new F.Point(300,200);
-        t.position = p;
-        t.text = "HELLO WORLD";
-        t.font = "100px Arial";
-        t.fillStyle = "yellow";
-        s2.addChild(t);
-
-        //falafel._nEditor.describeScene(falafel);
-
-        falafel.runScene(s._name);
-
-
-
-        requestAnimationFrame((dt)=>{falafel.run(dt)});
-
-        //falafel.run();
-
-    }
+  }
 }
